4ab8cdffb3944f23a2926be28c7e578545eefec0
bobu2525 <takadanobuyuki25@gmail.com>
2022-10-26 13:01:13 +0900

    タイピング内容変更

--- main.js
+++ main.js
@@ -12,20 +12,7 @@ const count = document.getElementById('count');
 
 // 複数のテキストを格納する配列
 const textLists = [
-  'Hello World','This is my App','How are you?',
-  'Today is sunny','I love JavaScript!','Good morning',
-  'I am Japanese','Let it be','Samurai',
-  'Typing Game','Information Technology',
-  'I want to be a programmer','What day is today?',
-  'I want to build a web app','Nice to meet you',
-  'Chrome Firefox Edge Safari','machine learning',
-  'Brendan Eich','John Resig','React Vue Angular',
-  'Netscape Communications','undefined null NaN',
-  'Thank you very much','Google Apple Facebook Amazon',
-  'ECMAScript','console.log','for while if switch',
-  'var let const','Windows Mac Linux iOS Android',
-  'programming'
-
+ 'ことな','けいな','のぶゆき','えり','家族',
 ];
 // ランダムなテキストを表示
 const createText = () => {
